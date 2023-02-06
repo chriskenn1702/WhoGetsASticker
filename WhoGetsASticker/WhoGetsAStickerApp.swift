fa16f85c1db85d6a1055203c6d73b3349aafa035
Christopher Kennedy <kennednu@bc.edu>
2023-02-06 17:50:22 -0500

    Initial Commit

---
+++ WhoGetsASticker/WhoGetsAStickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhoGetsAStickerApp.swift
+//  WhoGetsASticker
+//
+//  Created by Christopher Kennedy on 2/6/23.
+//
+
+import SwiftUI
+
+@main
+struct WhoGetsAStickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
