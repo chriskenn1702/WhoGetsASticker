ae569aaedfc092f2cabb60fc37a3ca91fe6399c9
Christopher Kennedy <kennednu@bc.edu>
2023-02-06 18:17:01 -0500

    Who gets a sticker weekly challenge

--- WhoGetsASticker/ContentView.swift
+++ WhoGetsASticker/ContentView.swift
@@ -8,12 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var students = ["Natasha", "Shuri", "Shaun", "Steve", "Tony", "Bruce"]
+    @State var student = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
+            Text("Who Gets a Sticker?")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundColor(.indigo)
+            
+            Image("MyMacBuildsApps")
+                .resizable()
+                .scaledToFit()
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            Spacer()
+            
+            Text("This Weeks Winner is:")
+                .font(.largeTitle)
+            
+            Spacer()
+            
+            Text(student)
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .frame(height: 10)
+            
+            Spacer()
+            Spacer()
+            
+            Button{
+                if students.isEmpty{
+                    student = "No one tooted this week :("
+                } else{
+                    student = students.randomElement()!
+                }
+            } label: {
+                Image(systemName: "person.crop.circle.badge.questionmark")
+                Text("Select Winner")
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.indigo)
         }
         .padding()
     }
